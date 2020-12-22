5b77f385f9931937977946813412817a0ccfbbca
Ivan Danko <vanjodan@gmail.com>
2020-12-22 08:50:40 +0200

    init

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,154 @@
+<template>
+  <q-layout view="hHh Lpr lff">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title
+          ><img
+            alt="Vostok logo"
+            src="~assets/logo.svg"
+            flat
+            @click="leftDrawerOpen = !leftDrawerOpen"
+        /></q-toolbar-title>
+        <img alt="Vostok logo" src="~assets/User.svg" flat />
+        <q-btn-dropdown stretch flat label="Name Secondname">
+          <q-list>
+            <q-item-label header>Folders</q-item-label>
+            <q-item
+              v-for="n in 1"
+              :key="`x.${n}`"
+              clickable
+              v-close-popup
+              tabindex="0"
+            >
+              <q-item-section avatar>
+                <q-avatar icon="folder" color="secondary" text-color="white" />
+              </q-item-section>
+              <q-item-section>
+                <q-item-label>Photos</q-item-label>
+                <q-item-label caption>February 22, 2016</q-item-label>
+              </q-item-section>
+              <q-item-section side>
+                <q-icon name="info" />
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-btn-dropdown>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      :mini="miniState"
+      @mouseover="miniState = false"
+      @mouseout="miniState = true"
+      :width="288"
+      :breakpoint="500"
+      bordered
+      content-class="bg-grey-3"
+    >
+      <q-scroll-area class="fit">
+        <q-list padding>
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="inbox" />
+            </q-item-section>
+
+            <q-item-section> Inbox </q-item-section>
+          </q-item>
+
+          <q-item active clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="star" />
+            </q-item-section>
+
+            <q-item-section> Star </q-item-section>
+          </q-item>
+
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="send" />
+            </q-item-section>
+
+            <q-item-section> Send </q-item-section>
+          </q-item>
+
+          <q-separator />
+
+          <q-item clickable v-ripple>
+            <q-item-section avatar>
+              <q-icon name="drafts" />
+            </q-item-section>
+
+            <q-item-section> Drafts </q-item-section>
+          </q-item>
+        </q-list>
+      </q-scroll-area>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from "components/EssentialLink.vue";
+
+const linksData = [
+  {
+    title: "Docs",
+    caption: "quasar.dev",
+    icon: "school",
+    link: "https://quasar.dev",
+  },
+  {
+    title: "Github",
+    caption: "github.com/quasarframework",
+    icon: "code",
+    link: "https://github.com/quasarframework",
+  },
+  {
+    title: "Discord Chat Channel",
+    caption: "chat.quasar.dev",
+    icon: "chat",
+    link: "https://chat.quasar.dev",
+  },
+  {
+    title: "Forum",
+    caption: "forum.quasar.dev",
+    icon: "record_voice_over",
+    link: "https://forum.quasar.dev",
+  },
+  {
+    title: "Twitter",
+    caption: "@quasarframework",
+    icon: "rss_feed",
+    link: "https://twitter.quasar.dev",
+  },
+  {
+    title: "Facebook",
+    caption: "@QuasarFramework",
+    icon: "public",
+    link: "https://facebook.quasar.dev",
+  },
+  {
+    title: "Quasar Awesome",
+    caption: "Community Quasar projects",
+    icon: "favorite",
+    link: "https://awesome.quasar.dev",
+  },
+];
+
+export default {
+  name: "MainLayout",
+  components: { EssentialLink },
+  data() {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData,
+      miniState: true,
+    };
+  },
+};
+</script>
